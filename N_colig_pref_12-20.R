36c5c3e31547db25ea0a780003fbf24fa8a70941
Guilherme A. Russo <38958006+guirusso@users.noreply.github.com>
2020-10-15 13:56:13 -0300

    Add files via upload

--- N_colig_pref_12-20.R
+++ N_colig_pref_12-20.R
@@ -54,6 +54,12 @@ head(pairs16)
 
 # 2020 data ----
 setwd("/Users/guilhermerusso/Downloads")
+
+#download.file("http://agencia.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_2020.zip",
+#              destfile = "consulta_cand_2020.zip")
+#unzip("consulta_cand_2020.zip", 
+#      exdir = "consulta_cand_2020")
+
 list.files(path="./consulta_cand_2020/")
 df20<-read.csv("consulta_cand_2020/consulta_cand_2020_BRASIL.csv", sep=";",
                encoding="latin1")
